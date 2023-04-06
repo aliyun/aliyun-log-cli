f65a11f3fd25cd86a79f24cf08ce6d28c5696b85
fanzhonghao.fzh <fanzhonghao.fzh@alibaba-inc.com>
2023-04-06 18:03:27 +0800

    clear code

--- aliyunlogcli/test.py
+++
@@ -1,7 +0,0 @@
-from aliyunlogcli.config import SUPPORT_LIST
-import itertools
-
-if __name__ == '__main__':
-    lists = SUPPORT_LIST.values()
-    result = [i for j in lists for i in j]
-    print(result)
